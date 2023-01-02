bd84c6d42320056d7769fc3ca68384d42092cfd0
GiselleBarbosa <g88barbosa@gmail.com>
2023-01-02 15:34:40 -0300

    created moment-form

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-moment-form',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./moment-form.component.scss']
 })
 export class MomentFormComponent implements OnInit {
-
+  @Input() btnText! : string;
+  srcResult: any;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+  
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+  
+      reader.onload = (e: any) => {
+        this.srcResult = e.target.result;
+      };
+  
+      reader.readAsArrayBuffer(inputNode.files[0]);
+    }
   }
 
 }
