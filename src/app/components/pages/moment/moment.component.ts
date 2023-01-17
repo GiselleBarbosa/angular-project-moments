e507923633ac14985c145ce5598933ec5c4cc63a
GiselleBarbosa <g88barbosa@gmail.com>
2023-01-17 08:53:20 -0300

    remove moment works!

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from '../../../services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
@@ -21,7 +23,10 @@ export class MomentComponent implements OnInit {
 
   constructor(
     private MomentService: MomentService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -30,5 +35,11 @@ export class MomentComponent implements OnInit {
       .getMoment(id)
       .subscribe((item) =>
         (this.moment = item.data));
+  }
+
+  async onRemove(id: number) {
+    await this.MomentService.removeMoment(id).subscribe();
+    this.messagesService.add("Momento excluido com sucesso!");
+    this.router.navigate(['/']);
   }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -23,11 +23,15 @@ export class MomentService {
   getMoment(id: number): Observable<Response<Moment>> {
     const url = `${this.apiUrl}/${id}`;
     return this.http.get<Response<Moment>>(url);
-
   }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  removeMoment(id: number) {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<FormData>(url);
   }
 
 }
